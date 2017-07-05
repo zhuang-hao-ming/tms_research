3176d631a4463f6a43c7ffe8c0b680068ce23c08
zhuang-hao-ming <csuhaoming@163.com>
2017-07-05 11:28:52 +0800

    add support to geodetic in vecotr tile map

--- vector/vector_tile.py
+++ vector/vector_tile.py
@@ -1,21 +1,33 @@
+# -*- encoding: utf-8
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import os
 
 import mapnik
-from sphericalmercator import SphericalMercator
 
+from globalmaptiles import GlobalMercator, GlobalGeodetic
 from osgeo import osr
 
-p1 = osr.SpatialReference()
-p1.ImportFromEPSG(4326)
-p2 = osr.SpatialReference()
-p2.ImportFromEPSG(3857)
-transform = osr.CoordinateTransformation(p1, p2)
 
+#profile = 'mercator'
+profile = 'geodetic'
+
+if profile == 'mercator':
+    map_width = 256
+    map_height = 256
+    map_srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs' 
+    merc = GlobalMercator()
+elif profile == 'geodetic':
+    map_width = 512
+    map_height = 256
+    map_srs = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+    merc = GlobalGeodetic()
 
 def get_map():
-    m = mapnik.Map(256, 256)
-    m.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs'
+    m = mapnik.Map(map_width, map_height)
+
+    
+    m.srs = map_srs # the output projection
+
     m.background = mapnik.Color('steelblue')
     s = mapnik.Style()
     r = mapnik.Rule()
@@ -33,7 +45,8 @@ def get_map():
     ds = mapnik.Shapefile(file='./ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp')
 
     layer = mapnik.Layer('world')
-    # layer.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs'
+    # layer.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs' # the input projection, it mush be the same as the source's projection
+    # '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
     layer.datasource = ds
     layer.styles.append('My Style')
     m.layers.append(layer)
@@ -41,7 +54,9 @@ def get_map():
 
 PORT_NUM = 8080
 m = get_map()
-merc = SphericalMercator(levels=18, size=256)
+
+
+
 
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -57,12 +72,33 @@ class MyHandler(BaseHTTPRequestHandler):
             y = int(arr[3])
 
         try: 
-
-            envelope1 = merc.xyz_to_envelope(x=x, y=y, zoom=z)                    
-            minx, miny, z = transform.TransformPoint(envelope1[0], envelope1[1])
-            maxx, maxy, z = transform.TransformPoint(envelope1[2], envelope1[3])
-            extent =  mapnik.Box2d(minx, miny, maxx, maxy)
             
+            # z/x/y 反算 bound：
+            # 计算地图分辨率：
+            # res = initRes / 2 ** z
+            # 计算瓦片角点到地图左上角的距离
+            # x * 256 * res = dist_min_x,
+            # y * 256 * res = dist_min_y ,
+            # (x + 1) * 256 * res = dist_max_x,
+            # (y + 1) * 256 * res = dist_max_y
+            # 计算瓦片角点投影坐标
+            # tile_x_min = map_x_min + dist_min_x
+            # tile_y_min = map_y_max - dist_min_y
+            # tile_x_max = map_x_min + dist_max_x
+            # tile_y_max = map_y_max - dist_max_y
+            # 反算wgs84坐标 
+            # 
+            # 由于在球面墨卡托投影中，我们令纵横分辨率相同，所以正方形瓦片覆盖的地理范围是正方形的，可以无变形的绘制到正方形瓦片上。 
+            # 
+            
+            if profile == 'mercator':
+                new_x, new_y = merc.GoogleTile(x, y, z)
+                minx, miny, maxx, maxy = merc.TileBounds(new_x, new_y, z)
+                extent =  mapnik.Box2d(minx, miny, maxx, maxy)
+            elif profile == 'geodetic':                
+                minx, miny, maxx, maxy = merc.TileBounds(x, y, z)
+                
+                extent =  mapnik.Box2d(minx, miny, maxx, maxy)
             
             
             m.zoom_to_box(extent)
